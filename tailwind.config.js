b8414385737e5d2854e0212f87bc17c9fddbced3
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2025-06-20 01:57:42 -0700

    Update MusicArtwork and SongPlayer styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,6 +42,10 @@ export default {
           DEFAULT: 'hsl(var(--destructive))',
           foreground: 'hsl(var(--destructive-foreground))',
         },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))'
